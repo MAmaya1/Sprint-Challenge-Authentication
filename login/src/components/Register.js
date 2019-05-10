d74e6559df24cac3edcc9756bcf76d4d977ef733
Mario Amaya <thefozcat@gmail.com>
2019-05-10 11:22:32 -0700

    styled Register component

--- login/src/components/Register.js
+++ login/src/components/Register.js
@@ -1,6 +1,53 @@
 import React from 'react';
 import axios from 'axios';
 
+import styled from 'styled-components';
+
+// Styled Components
+
+const RegisterPage = styled.div`
+    max-width: 700px;
+    height: 200px;
+    margin: auto;
+    background: white;
+    padding: 20px;
+    text-align: center;
+
+    h2 {
+        margin: 0 0 15px 0;
+    }
+`
+
+const RegisterForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    input {
+        width: 50%;
+        margin: 3px 0;
+        padding: 8px;
+        border: solid 1px black;
+        border-radius: 5px;
+    }
+
+    button {
+        margin-top: 20px;
+        width: 80px;
+        height: 30px;
+        background: white;
+        border: solid 1px black;
+        border-radius: 5px;
+
+        &:hover {
+            background: #D9E0E2;
+            transition: all 200ms ease;
+        }
+    }
+`
+
+// Register Component
+
 class Register extends React.Component {
     state = {
         credentials: {
@@ -38,9 +85,9 @@ class Register extends React.Component {
 
     render() {
         return (
-            <div className="sign-up-page">
+            <RegisterPage>
                 <h2>Sign Up</h2>
-                <form onSubmit={this.addUser}>
+                <RegisterForm onSubmit={this.addUser}>
                     <label htmlFor="username"/>
                     <input
                         id="username"
@@ -60,8 +107,8 @@ class Register extends React.Component {
                         onChange={this.handleChange}
                     />
                     <button type="submit">Submit</button>
-                </form>
-            </div>
+                </RegisterForm>
+            </RegisterPage>
         )
     }
 }
