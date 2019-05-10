65651ad1dc34b3714e518827140e750b754bbeba
Mario Amaya <thefozcat@gmail.com>
2019-05-10 10:58:42 -0700

    added styled-components, styled Header

--- login/src/components/Header.js
+++ login/src/components/Header.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import styled from 'styled-components';
+
+// Styled Components
+
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: flex-end;
+    background: #A4bCC4;
+    padding: 15px 10px;
+
+    a {
+        margin-left: 15px;
+        text-decoration: none;
+        color: #1F222B;
+        font-weight: bold;
+        
+        &:hover {
+            color: white;
+            transition: all 200ms ease;
+        }
+    }
+
+    .active {
+        color: white;
+    }
+
+`
+
+// Header Component
+
 const Header = props => {
     return (
-        <header>
+        <StyledHeader>
             <NavLink exact to="/login">Log In</NavLink>
             <NavLink to="/register">Sign Up</NavLink>
-        </header>
+        </StyledHeader>
     )
 }
 
